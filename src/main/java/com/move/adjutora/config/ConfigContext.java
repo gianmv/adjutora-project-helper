5b69acd5905493f0a262d05b73e7ee14f1484885
gianmv <gianmove@gmail.com>
2024-12-30 02:19:29 -0600

    delete unnecessary jline dependencies and add jackson lib for json handling

--- src/main/java/com/move/adjutora/config/ConfigContext.java
+++ src/main/java/com/move/adjutora/config/ConfigContext.java
@@ -1,9 +1,12 @@
 package com.move.adjutora.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.move.adjutora.service.TaskService;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -12,14 +15,20 @@ import java.util.Properties;
 public class ConfigContext {
     private final SqlSessionFactory sqlSessionFactory;
     private final TaskService taskService;
+    private final ObjectMapper mapper;
 
     public ConfigContext(String myBatisConfigLocation, Properties props) {
         try {
             InputStream inputStream = Resources.getResourceAsStream(myBatisConfigLocation);
 
             SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-            sqlSessionFactory= builder.build(inputStream, props);
-            taskService = new TaskService(sqlSessionFactory);
+            this.sqlSessionFactory= builder.build(inputStream, props);
+            this.taskService = new TaskService(sqlSessionFactory);
+
+            this.mapper = JsonMapper
+                    .builder()
+                    .addModule(new JavaTimeModule()).build();
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -27,5 +36,9 @@ public class ConfigContext {
 
     public TaskService getTaskService() {
         return taskService;
+    }
+
+    public ObjectMapper getMapper() {
+        return mapper;
     }
 }
